e9ee881f87a89e15a88872038050364726a46504
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2023-07-31 18:59:31 +0000

    feat(App): add dropdown component and premium calculation logic

---
+++ src/components/DropDown/index.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function DropDown({ labelData, options, handleChange }) {
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleDropdownChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <div>
+      <label>
+        {labelData}:
+        <select value={selectedOption} onChange={handleDropdownChange}>
+          {options.map((val, idx) => (
+            <option key={`${val}_${idx}`} value={val}>
+              {val}
+            </option>
+          ))}
+        </select>
+      </label>
+    </div>
+  );
+}
+
+export default DropDown;
